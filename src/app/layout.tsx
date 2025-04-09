7016496cd99a620a317a96620a8378175fb2aa03
owencairns <owencairns@me.com>
2025-04-09 09:31:57 -0400

    favicon and social media cards

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,33 @@ import QueryProvider from "@/providers/QueryProvider";
 export const metadata: Metadata = {
   title: "Skylimit Visuals",
   description: "Professional Wedding Videography Services",
+  icons: {
+    icon: "/img/logos/slv-logo-icon.webp",
+  },
+  metadataBase: new URL('https://skylimitvisuals.com'),
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://skylimitvisuals.com',
+    title: 'Skylimit Visuals',
+    description: 'Professional Wedding Videography Services',
+    siteName: 'Skylimit Visuals',
+    images: [
+      {
+        url: '/img/logos/slv-logo-icon.webp',
+        width: 1200,
+        height: 630,
+        alt: 'Skylimit Visuals Logo',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Skylimit Visuals',
+    description: 'Professional Wedding Videography Services',
+    images: ['/img/logos/slv-logo-icon.webp'],
+    creator: '@skylimitvisuals',
+  },
 };
 
 export default function RootLayout({
